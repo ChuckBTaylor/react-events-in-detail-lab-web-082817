924d790f545819a5a54785a2275fab6c56ab0dde
ChuckBTaylor <WLScripps@gmail.com>
2017-10-31 14:05:18 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,40 @@
-// Code CoordinatesButton Component Here
+import React from 'react';
+
+class CoordinateButtons extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      xCoord: null,
+      yCoord: null
+    }
+  }
+
+  handleOnClick(ev){
+    this.setState({
+      xCoord: ev.screenX,
+      yCoord: ev.screenY
+    })
+    const x = ev.screenX
+    const y = ev.screenY
+    this.props.onReceiveCoordinates([x,y])
+
+  }
+
+  render(){
+    return(
+      <button onClick={this.handleOnClick.bind(this)}>
+
+      </button>
+    )
+  }
+}
+
+CoordinateButtons.defaultProps = {
+
+}
+
+CoordinateButtons.propTypes = {
+
+}
+
+export default CoordinateButtons;
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,33 @@
-// Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+class DelayedButton extends Component{
+  constructor(){
+    super();
+  }
+
+  handleOnClick(e) {
+    e.persist()
+    setTimeout(() => {
+      this.props.onDelayedClick(e)
+    }, this.props.delay)
+  }
+
+
+  render(){
+    return(
+      <button onClick={this.handleOnClick.bind(this)}>
+
+      </button>
+    )
+  }
+}
+
+DelayedButton.defaultProps = {
+
+}
+
+DelayedButton.propTypes = {
+
+}
+
+export default DelayedButton;
